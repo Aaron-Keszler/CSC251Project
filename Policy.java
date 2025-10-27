a9814973ac452bbf69a4c2fbc82d9430496c4df6
Aaron Keszler <aakeszler@gmail.com>
2025-10-27 11:57:09 -0400

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -21,7 +21,17 @@ public class Policy {
       weight = 0.0;
    }
    
-   // arg constructor
+   /*
+   arg constructor
+   @param pNumber Policy number
+   @param pName Providers name
+   @param fName Policy holders first name
+   @param lName Policy holders last name
+   @param pAge Policy holders age
+   @param smokeStatus Does the policy holder smoke?
+   @param pHeight Policy holders height
+   @param pWeight Policy holders weight 
+   */
    public Policy(int pNumber, String pName, String fName, String lName, int pAge, String smokeStatus, double pHeight, double pWeight) {
          policyNumber = pNumber;
          providerName = pName;
@@ -34,7 +44,17 @@ public class Policy {
    }
    
    
-   // setters
+   /*
+   Setters
+   @param pNumber Sets policy number
+   @param pName Sets providers name
+   @param fName Sets policy holders first name
+   @param lName Sets policy holders last name
+   @param pAge Sets policy holders age
+   @param smokeStatus Sets policy holders smoking status
+   @param pHeight Sets policy holders height
+   @param pWeight Sets policy holders weight 
+   */
    public void setPolicyNumber(int pNumber) { policyNumber = pNumber; }
    public void setProviderName(String pName) { providerName = pName; }
    public void setFirstName(String fName) { firstName = fName; }
@@ -45,7 +65,17 @@ public class Policy {
    public void setWeight(double pWeight) { weight = pWeight; }
    
    
-   // getters
+     /*
+   Getters
+   @param pNumber Gets policy number
+   @param pName Gets providers name
+   @param fName Gets policy holders first name
+   @param lName Gets policy holders last name
+   @param pAge Gets policy holders age
+   @param smokeStatus Gets policy holders smoking status
+   @param pHeight Gets policy holders height
+   @param pWeight Gets policy holders weight 
+   */
    public int getPolicyNumber() { return policyNumber; }
    public String getProviderName() { return providerName; }
    public String getFirstName() { return firstName; }
@@ -55,12 +85,18 @@ public class Policy {
    public double getHeight() { return height; }
    public double getWeight() { return weight; }
    
-   // calculates policy holder BMI
+   /* 
+   calculates policy holder BMI
+   @return Returns the policy holders calculated BMI
+   */
    public double calculateBMI() {
       return (weight * 703) / (height * height);
    }
    
-   // calculates the price of the insurance policy
+   /*
+   calculates the price of the insurance policy
+   @return Returns the policy price after calculation
+   */
    public double calculatePolicyPrice() {
       double price = 600;
       
